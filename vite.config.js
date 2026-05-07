f30ca540c145eeb4240f89f9accc982843c7c6e6
T <tanimation2023@gmail.com>
2026-05-07 22:03:26 +0530

    Create vite.config.js

---
+++ vite.config.js
@@ -0,0 +1,27 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import path from 'path'
+
+export default defineConfig({
+  plugins: [react()],
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+    },
+  },
+  optimizeDeps: {
+    include: ['three', '@react-three/fiber', '@react-three/drei', 'gsap'],
+    exclude: ['locomotive-scroll'],
+  },
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          three: ['three', '@react-three/fiber', '@react-three/drei'],
+          gsap: ['gsap'],
+          motion: ['framer-motion'],
+        },
+      },
+    },
+  },
+})
